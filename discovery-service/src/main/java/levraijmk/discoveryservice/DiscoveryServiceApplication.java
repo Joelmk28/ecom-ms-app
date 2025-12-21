3988758929bc82fee1d48247798fa52ad29740d0
Joelmk28 <joelmuhindok@gmail.com>
2025-12-21 07:00:59 +0100

    Activation de la communication avec discovery grace au paramettre spring.cloud.discovery.enabled

---
+++ discovery-service/src/main/java/levraijmk/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package levraijmk.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //pour activer le serveur Eureka
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
