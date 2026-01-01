f71cb35ad9160ceb187a2cd5673e23c68ab79f5b
Joelmk28 <joelmuhindok@gmail.com>
2026-01-01 06:22:29 +0100

    entities

---
+++ billing-service/src/main/java/levraijmk/billingservice/entities/Bill.java
@@ -0,0 +1,25 @@
+package levraijmk.billingservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private Date billingDate;
+    private long customerId;
+    @OneToMany(mappedBy = "bill")
+    private List<ProductItem> productItems;
+}
---
+++ billing-service/src/main/java/levraijmk/billingservice/entities/ProductItem.java
@@ -0,0 +1,21 @@
+package levraijmk.billingservice.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class ProductItem {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private long productId;
+    private int quantity;
+    private double price;
+    @ManyToOne
+    private Bill bill;
+}
