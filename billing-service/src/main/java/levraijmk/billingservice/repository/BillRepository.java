c65e932a6cdbbeb985ecb03a70c2adb62f56e802
Joelmk28 <joelmuhindok@gmail.com>
2026-01-01 07:03:09 +0100

    Ajout de l'annotation RespositoryrestResource

--- billing-service/src/main/java/levraijmk/billingservice/repository/BillRepository.java
+++ billing-service/src/main/java/levraijmk/billingservice/repository/BillRepository.java
@@ -2,6 +2,9 @@ package levraijmk.billingservice.repository;
 
 import levraijmk.billingservice.entities.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource
 public interface BillRepository extends JpaRepository<Bill, Long> {
 }
--- billing-service/src/main/java/levraijmk/billingservice/repository/ProductItemRepository.java
+++ billing-service/src/main/java/levraijmk/billingservice/repository/ProductItemRepository.java
@@ -2,9 +2,10 @@ package levraijmk.billingservice.repository;
 
 import levraijmk.billingservice.entities.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-
+@RepositoryRestResource
 public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
     List<ProductItem> findByBillId(Long billId);
 }
